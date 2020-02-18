a1a264b669b45c278d43e5b0cac4ba8c1b3bcb2c
Dmitriy <50251075+oruruby@users.noreply.github.com>
2020-02-18 19:01:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 export default function fragment(node) {
-  node.parentElement.appendChild(node.content);
-  node.setAttribute('style', 'display: none;');
-
+  const parent = node.parentElement;
+  const children = Array.prototype.slice.call(
+    (node.content || node).childNodes,
+  );
+  if (!node.content || node.content.nodeType !== 11) {
+    node.content = document.createDocumentFragment();
+    for (let i in children) {
+      node.content.appendChild(children[i]);
+    }
+  }
+  parent.appendChild(node.content);
+  document.createDocumentFragment().appendChild(document.adoptNode(node));
   return {
     destroy() {
-      if (node && node.parentElement) {
-        node.parentElement.removeChild(node.content);
-      }
+     requestAnimationFrame(function() {
+        for (let i in children) {
+          if (parent && parent.contains(children[i])) {
+            parent.removeChild(children[i]);
+          }
+        }
+      });
     },
   };
 }
